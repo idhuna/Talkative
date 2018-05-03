aeb2e6435807da62d01371b01948a606efd5eab2
Thanthawat Suwannachatkul <peace_under@hotmail.com>
2018-05-03 14:28:20 +0700

    update app.js, use this temporary

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mongoose = require('mongoose');
+
+//connect to mongoDB
+mongoose.connect('mongodb://localhost/test');
+require('./model/group-model');
+require('./model/client-model.js');
+require('./model/message-model.js');
 
 var app = express();
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var groupRouter = require('./routes/group');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/group', groupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
