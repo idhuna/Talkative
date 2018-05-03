30b9e3445feec72f9f3d3569f1784a690996d449
Poomchai Taechaprapasrat <poomchai@3dsinteractive.com>
2018-05-03 15:36:08 +0700

    add create user route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
+var Client = require('mongoose').model('Client');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/createuser', async function (req, res, next) {
+  console.log(req.body);
+
+  const { clientID, password, name } = req.body;
+  let newUser = new Client({
+    clientID: clientID,
+    password: password,
+    name: name,
+  });
+  try {
+    await newUser.save();
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+  res.send(newUser)
+
+});
+
+
 
 module.exports = router;
