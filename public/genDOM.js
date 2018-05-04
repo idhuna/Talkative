1899e08cfd4b3a8e229d667b9ef7db85e443e0a9
waiiinta <waiiboy@hotmail.com>
2018-05-04 14:47:06 +0700

    edit genDOM.js , joinTopic button can send form now

---
+++ public/genDOM.js
@@ -0,0 +1,35 @@
+// $('#addTopicBtn').click(()=>{
+//     var topic = $('#nameTopic').val 
+//     console.log("hi",topic)
+//     if(topic.length > 0){
+//         createGroup
+//     }
+// })
+
+const joinGroup = () =>{
+    let groupName = $('#joinedTopic').val()
+    fetch('users/joingroup',{
+      method: "POST",
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        clientID:clientID,
+        groupName:groupName
+      })
+    }).then(res => res.text()).then(data => console.log(data))
+  }
+
+$('#joinedGroup').submit((e) => {
+    e.preventDefault()
+    joinGroup()
+})
+
+$('#joinTopicBtn').click(()=>{
+    var topic = $('#joinedTopic').val 
+    console.log("hi",topic)
+    if(topic.length > 0){
+        joinGroup()
+    }
+})
